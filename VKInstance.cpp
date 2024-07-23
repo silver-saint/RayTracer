7041438cd00492669860d504aab2f9b40b68e290
silver-saint <naydenovneyko@gmail.com>
2024-07-23 20:28:04 +0300

    refactored some code

--- VKInstance.cpp
+++ VKInstance.cpp
@@ -287,10 +287,10 @@ void VKApp::CreateInstance()
 	VkApplicationInfo appInfo = {};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pApplicationName = "RayTracer";
-	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 3, 0);
 	appInfo.pEngineName = "No Engine";
-	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.apiVersion = VK_API_VERSION_1_0;
+	appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 3, 0);
+	appInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 3, 0);
 
 	VkInstanceCreateInfo createInfo = {};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
