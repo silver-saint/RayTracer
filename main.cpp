fba378acfab8e6b8ff7a4fcca6c9463c818c0f31
silver-saint <naydenovneyko@gmail.com>
2024-09-24 01:19:23 +0300

    Revert "stopped toying around"
    This reverts commit d23bd22f63da89d9db55b20ccb7f5bac0bb5a4e8.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
-#include "App/App.h"
-#include "../DebugLayer.h"
+//#include "App/App.h"
 #include "../platform/WinInstance.h"
+#include <iostream>
+#include <DirectXMath.h>
 
 
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    PWSTR pCmdLine, int nCmdShow) 
-{
-    DX::DebugLayer debug;
-    /*
+    PWSTR pCmdLine, int nCmdShow) {
+    DirectX::XMVECTOR u = { -1,3,2 };
+    DirectX::XMVECTOR v = { 3,-4,1 };
+    DirectX::XMVECTOR resu = DirectX::XMVector3Normalize(u);
+    DirectX::XMVECTOR resv = DirectX::XMVector3Normalize(v);
+
+/*
     dx::engine::App app;
     return EXIT_SUCCESS;
-    */
+  */  
 }
 
 
